7152e73bb6e3e8a3c505cb1fd1a34a592ef259f0
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-07-13 18:49:12 +0530

    code refactor using class method

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -1,17 +1,16 @@
 package com.bridgelabz;
 
 public class EmployeeWages {
-    public static void main(String[] args)
+    final int WAGE_PER_HOUR=20;
+    final int FULL_DAY_HOUR=8;
+    final int HALF_DAY_HOUR=4;
+    final int ABSENT=0;
+    final int FULL_TIME=1;
+    final int PART_TIME=2;
+    final int WORKING_DAYS_PER_MONTH=20;
+    final int TOTAL_WORKING_HOURS=100;
+    void calculateWages()
     {
-        System.out.println("Welcome to Employee wage computation program");
-        final int WAGE_PER_HOUR=20;
-        final int FULL_DAY_HOUR=8;
-        final int HALF_DAY_HOUR=4;
-        final int ABSENT=0;
-        final int FULL_TIME=1;
-        final int PART_TIME=2;
-        final int WORKING_DAYS_PER_MONTH=20;
-        final int TOTAL_WORKING_HOURS=100;
         int dailyWage=0;
         int monthlyWages=0;
         int days=1;
@@ -47,7 +46,12 @@ public class EmployeeWages {
         }
         System.out.println("Monthly wages of Employee "+monthlyWages);
         System.out.println("Total working hours of Employee "+totalHours);
-
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Employee wage computation program");
+        EmployeeWages employeeWages=new EmployeeWages();
+        employeeWages.calculateWages();
     }
 
 }
